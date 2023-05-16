2b0bdebc6b6ff77d13ca0e8fb7bfa5c0c2e2d59c
mannybennett <mannybennett99@gmail.com>
2023-05-15 22:00:06 -0400

    First Commit

---
+++ src/UserCard.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function UserCard() {
+
+  const [arrayOfUsers, setArrayOfUsers] = useState([]);
+  const [clicked, setClick] = useState(false);
+  const [activeUser, setActiveUser] = useState({});
+  
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const userData = await axios.get('https://randomuser.me/api?results=25');
+      setArrayOfUsers(userData.data.results);
+    };
+    
+    fetchUserData();
+  }, []);
+
+  return (
+    <div className="container">
+      {arrayOfUsers.map((user, idx) => (
+        <Card key={idx} style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={user.picture.large} />
+        <Card.Body>
+          <Card.Title>{`${user.name.first} ${user.name.last}`}</Card.Title>
+          <Card.Text>
+            {clicked && idx === activeUser.idx && `${activeUser.gender} - ${activeUser.dob.age}`}
+          </Card.Text>
+          <Button onClick={() => {
+              setActiveUser({...user, idx})
+              clicked ? setClick(false) : setClick(true)
+            }} variant="danger">
+            {!clicked && idx === activeUser.idx ? "Show Details" : "Hide Details"}
+          </Button>
+        </Card.Body>
+      </Card>
+      ))}
+      
+    </div>
+  );
+}
+
+export default UserCard;
